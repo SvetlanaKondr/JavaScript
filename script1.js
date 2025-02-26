6f841edf7ad7e22c56d0c26ece2b47e2395f38c1
SvetlanaKondr <kanffeettka9@gmail.com>
2025-02-26 18:28:21 +0200

    updates

--- script1.js
+++ script1.js
@@ -6,3 +6,11 @@ for (let i=0; bilietai.length > i; i++){
     console.log(bilietai[i])
 }
 
+console.log(bilietai.length);
+
+
+let vardai = ['a', 'b', 'c'];
+console.log(vardai[0])
+console.log(vardai[1])
+console.log(vardai[2])
+console.log(vardai[3]) //undefined
